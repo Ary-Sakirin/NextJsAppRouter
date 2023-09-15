7aa871996038d3e4d6b1c5a6bed5db345b4f70b5
Ary-Sakirin <arysakirin29@gmail.com>
2023-09-15 16:36:44 +0700

    test2

--- src/screens/Home/components/Products/newsProducts.tsx
+++ src/screens/Home/components/Products/newsProducts.tsx
@@ -6,10 +6,52 @@ export default function NewProducts() {
             <h1 className="text-[25px] font-bold px-2 py-2">New products</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 <CardProducts
-                    title="Shose"
+                    title="Shoes"
                     status="New"
-                    image="ss"
-                    description="ss"
+                    image="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-mens-shoes-JHpxkn.png"
+                    description="If a dog chews shoes whose shoes does he choose?"
+                />
+                <CardProducts
+                    title="Shoes"
+                    status="New"
+                    image="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-mens-shoes-JHpxkn.png"
+                    description="If a dog chews shoes whose shoes does he choose?"
+                />
+                <CardProducts
+                    title="Shoes"
+                    status="New"
+                    image="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-mens-shoes-JHpxkn.png"
+                    description="If a dog chews shoes whose shoes does he choose?"
+                />
+                <CardProducts
+                    title="Shoes"
+                    status="New"
+                    image="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-mens-shoes-JHpxkn.png"
+                    description="If a dog chews shoes whose shoes does he choose?"
+                />
+                <CardProducts
+                    title="Shoes"
+                    status="New"
+                    image="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-mens-shoes-JHpxkn.png"
+                    description="If a dog chews shoes whose shoes does he choose?"
+                />
+                <CardProducts
+                    title="Shoes"
+                    status="New"
+                    image="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-mens-shoes-JHpxkn.png"
+                    description="If a dog chews shoes whose shoes does he choose?"
+                />
+                <CardProducts
+                    title="Shoes"
+                    status="New"
+                    image="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-mens-shoes-JHpxkn.png"
+                    description="If a dog chews shoes whose shoes does he choose?"
+                />
+                <CardProducts
+                    title="Shoes"
+                    status="New"
+                    image="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-mens-shoes-JHpxkn.png"
+                    description="If a dog chews shoes whose shoes does he choose?"
                 />
             </div>
         </>
